3d7e1b15fe4d66b8e75c4233f6ff3a7f758249fa
Lisa Linux <mclisa00@gmail.com>
2022-07-07 14:33:58 +0100

    Improved layout. Added random colour functionality. Added pen colour chooser.

--- main.js
+++ main.js
@@ -1,36 +1,40 @@
+// ELEMENTS
 const sketchpadBody = document.getElementById('pad-body');
-const maxWidth = 400;
+const sizeSlider = document.getElementById('size-slider');
+const sizeSliderLabel = document.getElementById('size-label');
+const colorPicker = document.getElementById('pen-color');
+const btnRandomColor = document.getElementById('btnRandomColor');
 
-// BUTTONS
-document.getElementById('btnDraw').addEventListener('click', refreshGrid);
-document.getElementById('btnClear').addEventListener('click', clearCells);
+const padWidth = 400; // used to determine cell size
+newGrid() // create initial grid when page loads
 
 
-function clearCells() {
+// CONTROL BUTTONS
+function toggle(button){
+    if (button.target.value==="ON") {
+        button.target.value="OFF";
+    } else {
+        button.target.value="ON";
+    }
+}
+
+document.getElementById('btnDraw').addEventListener('click', newGrid);
+document.getElementById('btnClear').addEventListener('click', clearCellFill);
+colorPicker.addEventListener('change', updateColor);
+btnRandomColor.addEventListener('click', toggle);
+sizeSlider.oninput = function() {sizeSliderLabel.textContent = `${this.value} x ${this.value}`};
+
+
+function clearCellFill() {
     document.querySelectorAll('.grid-cell').forEach(element => {
-        element.classList.remove('filled-cell');
+        element.style.backgroundColor = '';
     });
 }
 
 
-function makeGrid() {
-    const gridSize = setGridSize();  
-    for (i = 1; i <= (gridSize * gridSize); i++) {
-        const cell = makeCell();
-        setCellSize(cell, gridSize);
-    }
-}
-
-function makeCell() {
-    const newCell = document.createElement('div');
-    newCell.addEventListener('mouseover', () => {mouseCell(newCell)});
-    sketchpadBody.appendChild(newCell).classList.add('grid-cell');
-    return newCell;
-}
-
-
+// GRID CREATION
 function setGridSize() {
-    const gridSize = document.getElementById('grid-size').value;
+    const gridSize = sizeSlider.value;
     if (gridSize < 101 && gridSize > 0) {
         sketchpadBody.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
         sketchpadBody.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
@@ -40,25 +44,46 @@ function setGridSize() {
     }
 }
 
+
+function makeCell() {
+    const newCell = document.createElement('div');
+    newCell.addEventListener('mouseover', () => {mouseCell(newCell)});
+    sketchpadBody.appendChild(newCell).classList.add('grid-cell');
+    return newCell;
+}
+
+
 function setCellSize(cell, gridSize) {
-    cell.style.padding = `${maxWidth/gridSize}px`;
+    cell.style.minHeight = `${padWidth/gridSize}px`;
+    cell.style.minWidth = `${padWidth/gridSize}px`;
 }
 
 
-function refreshGrid() {
+function newGrid() {
     document.querySelectorAll('.grid-cell').forEach(cell => cell.remove());
-    makeGrid()
+    const gridSize = setGridSize();  
+    for (i = 1; i <= (gridSize * gridSize); i++) {
+        const cell = makeCell();
+        setCellSize(cell, gridSize);
+    }
 }
 
+
+// COLOURING
+let penColor = 'slateblue';
+const colorOptions = ["blue", "green", "red", "yellow"];
 
 function mouseCell(cell) {
-    cell.classList.add('filled-cell');
-    cell.style.backgroundColor = randomColour();
+    if (btnRandomColor.value === 'ON') {
+        cell.style.backgroundColor = colorOptions[Math.floor(Math.random()*colorOptions.length)]
+    } else {
+        cell.style.backgroundColor = penColor;
+    }
+    
 }
 
 
-function randomColour() {
-    const options = ["blue", "green", "red", "yellow"];
-    return options[Math.floor(Math.random()*options.length)];
+function updateColor(event) {
+    penColor = event.target.value;
+    document.getElementById('pen-dot').style.backgroundColor = penColor;
 }
-
